925d4085ed68a66b7494db77bdecc5cd82a05004
Jhonatan David Galindo Gomez <jhonatandgalindo@unicesar.edu.co>
2026-08-17 11:31:42 -0500

    feat: Metodo CREATE añadido a la Clase Main

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -1,16 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author Minec
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
+    static ArrayList<Client> clients = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
     }
+    
+    static void CREATE(){
+        String id,name,cellphone,mail;
+        System.out.println("\n### Nuevo cliente ###\n");
+        System.out.println("Ingrese su ID: ");
+        id = sc.nextLine();
+        System.out.println("Ingrese su nombre: ");
+        name = sc.nextLine();
+        System.out.println("Ingrese su numero de telefono: ");
+        cellphone = sc.nextLine();
+        System.out.println("Ingrese su correo electronico: ");
+        mail = sc.nextLine();
+        Client client = new Client(id,name,cellphone,mail);
+        clients.add(client);
+        System.out.println("Cliente registrado correctamente");
+    }
+    
 }
