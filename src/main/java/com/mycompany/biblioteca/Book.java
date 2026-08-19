50474225c6be5951935428b7b7f98303be79fb5f
Jhonatan David Galindo Gomez <jhonatandgalindo@unicesar.edu.co>
2026-08-18 19:28:18 -0500

    feat: clases Material preparadas como padre y Book como hija de Material(Clases creadas junto a la creacion del proyecto y la clase Main) / AHORA SI DE VERDAD.

--- src/main/java/com/mycompany/biblioteca/Book.java
+++ src/main/java/com/mycompany/biblioteca/Book.java
@@ -1,6 +1,15 @@
 
 package com.mycompany.biblioteca;
 
-public class Book {
+public class Book extends Material {
+    public String author;
+    public boolean available;
+
+    public Book(String code, String title, String publicationYear,
+                String author, boolean available) {
+        super(code, title, publicationYear);
+        this.author = author;
+        this.available = available;
+    }
     
 }
--- src/main/java/com/mycompany/biblioteca/Material.java
+++ src/main/java/com/mycompany/biblioteca/Material.java
@@ -2,5 +2,12 @@
 package com.mycompany.biblioteca;
 
 public class Material {
+    public String code, title, publicationYear;
+
+    public Material(String code, String title, String publicationYear) {
+        this.code = code;
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
     
 }
