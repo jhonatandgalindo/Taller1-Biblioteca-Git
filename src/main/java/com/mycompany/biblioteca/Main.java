64234bb5bbf6d2a77525ba3262eb65f5ebaf6d55
Jhonatan David Galindo Gomez <jhonatandgalindo@unicesar.edu.co>
2026-08-18 20:57:03 -0500

    feat: implementar UPDATE_BOOK de Book

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -10,13 +10,17 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        CREATE_BOOK();CREATE_BOOK();CREATE_BOOK();
+        CREATE_BOOK();
+        CREATE_BOOK();
+        CREATE_BOOK();
         BOOK_LIST();
-        SEARCH_BOOK();
+        String code=SEARCH_BOOK();
+        UPDATE_BOOK(code);
+        BOOK_LIST();
     }
-    
-    static void CREATE_CLIENT(){
-        String id,name,cellphone,mail;
+
+    static void CREATE_CLIENT() {
+        String id, name, cellphone, mail;
         System.out.println("\n### Nuevo cliente ###\n");
         System.out.println("Ingrese su ID: ");
         id = sc.nextLine();
@@ -26,12 +30,13 @@ public class Main {
         cellphone = sc.nextLine();
         System.out.println("Ingrese su correo electronico: ");
         mail = sc.nextLine();
-        Client client = new Client(id,name,cellphone,mail);
+        Client client = new Client(id, name, cellphone, mail);
         clients.add(client);
         System.out.println("Cliente registrado correctamente");
     }
-    static void CREATE_BOOK(){
-        String code,title,publicationYear,author;
+
+    static void CREATE_BOOK() {
+        String code, title, publicationYear, author;
         boolean available;
         System.out.println("\n### Registrar Libro ###\n");
         System.out.println("Ingrese codigo:");
@@ -43,91 +48,117 @@ public class Main {
         System.out.println("Ingrese autor:");
         author = sc.nextLine();
         System.out.println("Disponible?[true(SI)|false(NO)]:");
-        available = sc.nextBoolean();sc.nextLine();
-        Book book = new Book(code,title,publicationYear,author,available);
+        available = sc.nextBoolean();
+        sc.nextLine();
+        Book book = new Book(code, title, publicationYear, author, available);
         books.add(book);
         System.out.println("Libro registrado correctamente");
     }
-    static void CLIENT_LIST(){
+
+    static void CLIENT_LIST() {
         System.out.println("\n### Listado clientes ###\n");
         System.out.println("ID\t\tNOMBRE\t\tTELEFONO\t\tCORREO");
-        for(Client c : clients){
-            System.out.println(c.id+"\t\t"+c.name+"\t\t"+c.cellphone+"\t\t"+c.mail);
+        for (Client c : clients) {
+            System.out.println(c.id + "\t\t" + c.name + "\t\t" + c.cellphone + "\t\t" + c.mail);
         }
     }
-    static void BOOK_LIST(){
+
+    static void BOOK_LIST() {
         System.out.println("\n### Listado libros ###\n");
         System.out.println("CODIGO - TITULO - ANIO PUBLICACION - AUTOR - DISPONIBLE");
-        for(Book b : books){
+        for (Book b : books) {
             String available;
-            if(b.available){
-                available="Si";
-            }else{
-                available="No";
+            if (b.available) {
+                available = "Si";
+            } else {
+                available = "No";
             }
-            System.out.println(b.code+" - "+b.title+" - "+b.publicationYear+" - "+b.author+" - "+available);
+            System.out.println(b.code + " - " + b.title + " - " + b.publicationYear + " - " + b.author + " - " + available);
         }
     }
-    static String SEARCH_CLIENT(){
+
+    static String SEARCH_CLIENT() {
         String id;
         System.out.println("\n### Buscar Cliente ###\n");
         System.out.println("Introduzca el ID del cliente:");
         id = sc.nextLine();
-        for(Client c : clients){
-            if(c.id.equals(id)){
+        for (Client c : clients) {
+            if (c.id.equals(id)) {
                 System.out.println("Cliente encontrado:");
                 System.out.println("ID\t\tNOMBRE\t\tTELEFONO\t\tCORREO");
-                System.out.println(c.id+"\t\t"+c.name+"\t\t"+c.cellphone+"\t\t"+c.mail);
+                System.out.println(c.id + "\t\t" + c.name + "\t\t" + c.cellphone + "\t\t" + c.mail);
                 return id;
             }
         }
         System.out.println("Cliente NO encontrado");
         return null;
     }
-    static String SEARCH_BOOK(){
+
+    static String SEARCH_BOOK() {
         String code;
         System.out.println("\n### Buscar libro ###\n");
         System.out.println("Introduzca el codigo del libro:");
         code = sc.nextLine();
-        for(Book b : books){
+        for (Book b : books) {
             String available;
-            if(b.available){
-                available="Si";
-            }else{
-                available="No";
+            if (b.available) {
+                available = "Si";
+            } else {
+                available = "No";
             }
-            if(b.code.equals(code)){
+            if (b.code.equals(code)) {
                 System.out.println("Libro encontrado:");
                 System.out.println("CODIGO - TITULO - ANIO PUBLICACION - AUTOR - DISPONIBLE");
-                System.out.println(b.code+" - "+b.title+" - "+b.publicationYear+" - "+b.author+" - "+available);
+                System.out.println(b.code + " - " + b.title + " - " + b.publicationYear + " - " + b.author + " - " + available);
                 return code;
             }
         }
         System.out.println("Libro NO encontrado");
         return null;
     }
-    static void UPDATE_CLIENT(String id){
-        
-        for(Client c : clients){
-            
-            if(c.id.equals(id)){
+
+    static void UPDATE_CLIENT(String id) {
+
+        for (Client c : clients) {
+
+            if (c.id.equals(id)) {
                 System.out.println("\n### Actualizar Datos del Cliente ###\n");
                 System.out.println("ID:");
-                c.id=sc.nextLine();
+                c.id = sc.nextLine();
                 System.out.println("Nombre:");
-                c.name=sc.nextLine();
+                c.name = sc.nextLine();
                 System.out.println("Telefono:");
-                c.cellphone=sc.nextLine();
+                c.cellphone = sc.nextLine();
                 System.out.println("Correo electronico:");
-                c.mail=sc.nextLine();
+                c.mail = sc.nextLine();
                 System.out.println("DATOS DEL CLIENTE ACTUALIZADOS CORRECTAMENTE");
             }
         }
     }
-    static void DELETE_CLIENT(String id){
-        for(Client c : clients){
-            int i=0;
-            if(c.id.equals(id)){
+
+    static void UPDATE_BOOK(String code) {
+        for (Book b : books) {
+            if (b.code.equals(code)) {
+                System.out.println("\n### Actualizar Datos del Libro ###\n");
+                System.out.println("Ingrese codigo:");
+                b.code = sc.nextLine();
+                System.out.println("Ingrese titulo:");
+                b.title = sc.nextLine();
+                System.out.println("Ingrese anio de publicacion:");
+                b.publicationYear = sc.nextLine();
+                System.out.println("Ingrese autor:");
+                b.author = sc.nextLine();
+                System.out.println("Disponible?[true(SI)|false(NO)]:");
+                b.available = sc.nextBoolean();
+                sc.nextLine();
+            }
+        }
+    }
+
+    static void DELETE_CLIENT(String id) {
+        for (Client c : clients) {
+            int i = 0;
+            if (c.id.equals(id)) {
                 System.out.println("\n### Actualizar Datos del Cliente ###\n");
                 clients.remove(i);
                 System.out.println("CLIENTE ELIMINADO CORRECTAMENTE");
@@ -135,5 +166,5 @@ public class Main {
             }
         }
     }
-    
+
 }
